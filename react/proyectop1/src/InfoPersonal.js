458a1f7280d6d24aaeb1e527b34739e46d960ea2
Kay-Rodriguez <151900296+Kay-Rodriguez@users.noreply.github.com>
2024-12-17 09:05:03 -0500

    InfoPersonal.js

--- react/proyectop1/src/InfoPersonal.js
+++ react/proyectop1/src/InfoPersonal.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PersonalInfo = () => {
     return (
-        <div>
+    <div>
             <h2>Datos Personales</h2>
-            <p>Nombre: Karla Rodriguez</p>
-            <p>Dirección: Mitad del mundo calle museo solar trasversal calle B casa c7</p>
-            <p>Teléfono: 0987991372</p>
-            <p>Correo Institucional: karla.rodriguez01@epn.edu.ec</p>
+            <p> <strong>Nombre:</strong> Karla Rodriguez</p>
+            <p> <strong> Dirección: </strong> Mitad del mundo calle museo solar trasversal calle B casa c7</p>
+            <p> <strong> Teléfono:  </strong>0987991372</p>
+            <p> <strong> Correo Institucional:</strong>  karla.rodriguez01@epn.edu.ec</p>
         </div>
     );
 };
